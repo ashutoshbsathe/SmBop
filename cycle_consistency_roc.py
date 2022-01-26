5e720d6f49be3076613d23d6316446a746cae473
Ashutosh Sathe <2ashutoshbs@gmail.com>
2022-01-26 21:32:09 +0530

    Outputting dev accuracy with thresholds too

--- cycle_consistency_roc.py
+++ cycle_consistency_roc.py
@@ -1,13 +1,18 @@
 import json
 from sklearn.metrics import roc_curve, auc 
+import numpy as np 
 
 with open('./train_entries_list.json') as f:
     spider = json.load(f)
-    spider = [{'exec': 1 if d['exec'] else 0, 'partial': sum([d['partial'][k]['f1'] for k in d['partial']])} for d in spider]
+    spider = [{'exec': 1 if 'exec' in d and d['exec'] else 0, 'partial': sum([d['partial'][k]['f1'] for k in d['partial']]) if 'partial' in d else 0} for d in spider]
 
 with open('./train_negative_entries_list.json') as f:
     negative = json.load(f)
-    negative = [{'exec': 1 if d['exec'] else 0, 'partial': sum([d['partial'][k]['f1'] for k in d['partial']])} for d in negative]
+    negative = [{'exec': 1 if 'exec' in d and d['exec'] else 0, 'partial': sum([d['partial'][k]['f1'] for k in d['partial']]) if 'partial' in d else 0} for d in negative]
+
+with open('./dev_entries_list.json') as f:
+    dev = json.load(f)
+    dev = [{'exec': 1 if 'exec' in d and d['exec'] else 0, 'partial': sum([d['partial'][k]['f1'] for k in d['partial']]) if 'partial' in d else 0} for d in dev]
 
 for exec_multiplier in [1, 2, 4, 8, 16]:
     y = []
@@ -20,4 +25,14 @@ for exec_multiplier in [1, 2, 4, 8, 16]:
         scores.append(exec_multiplier * x['exec'] + x['partial'])
 
     fpr, tpr, threshold = roc_curve(y, scores)
-    print(auc(fpr, tpr))
+    print('AUC', auc(fpr, tpr))
+    optimal_threshold = threshold[np.argmax(tpr - fpr)]
+    print('Optimal threshold', optimal_threshold)
+
+    correct = 0
+    for x in dev:
+        if exec_multiplier * x['exec'] + x['partial'] >= optimal_threshold:
+            correct += 1
+
+    print('Dev set accuracy', correct / len(dev))
+    print(64*'-')
